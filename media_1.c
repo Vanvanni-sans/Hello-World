1f020623e2adcb0a43a9527a6aef8b877f0df8b0
Vanvanni-sans <giosansana12@gmail.com>
2024-07-02 18:37:49 -0300

    Create media_1.c

---
+++ media_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+   
+   double na, nb, media;
+   
+   scanf("%lf %lf", &na, &nb);
+   
+   media = ((na*3.5) + (nb*7.5))/11;
+   
+      printf("MEDIA = %.5lf\n",media);
+
+   
+     return 0;
+}

2404dd7e44c1af1ea1b3fc3f7c59197dfd312917
Vanvanni-sans <giosansana12@gmail.com>
2024-07-24 12:37:33 -0300

    Create impar.c

---
+++ impar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+
+  int a;
+  scanf("%d", &a);
+  for (int i = 0; i < 6;i++) {
+    if (a % 2 == 0) {
+      a += 1;
+      printf("%d\n", a);
+      a += 2;
+    } else if (a % 2 == 1) {
+      printf("%d\n", a);
+      a+=2;
+    }
+  }
+}

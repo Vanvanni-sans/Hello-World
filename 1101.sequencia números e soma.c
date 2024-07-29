ad0a5c28aa70e306b1dc961528fd620b49647c8d
Vanvanni-sans <giosansana12@gmail.com>
2024-07-29 15:42:13 -0300

    Create 1101.sequencia números e soma.c

---
+++ 1101.sequencia números e soma.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main() {
+int x, y, min, max,count=0;
+  for(int j = 0; j<1;)
+    {
+      scanf("%d %d", &x, &y);
+      if(x<=0 || y<=0){
+        return 0;
+      }
+      else {
+  if(x<y)
+  {
+    min=x;
+    max=y;
+  }
+  else
+    {
+      min=y;
+      max=x;
+    }
+  for (int i = min; i <= max; i++){
+  printf("%d ", i);   
+    count += i;
+  }
+  printf("Sum=%d\n", count);
+        count=0;
+    }
+    }
+  return 0;
+}

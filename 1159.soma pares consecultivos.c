5d7dad9d3d5cdc53d11709088757317145aaed72
Vanvanni-sans <giosansana12@gmail.com>
2024-08-05 16:43:12 -0300

    Update 1159.soma pares consecultivos.c

--- 1159.soma pares consecultivos.c
+++ 1159.soma pares consecultivos.c
@@ -3,24 +3,17 @@ int main() {
   int n, count = 0;
   for (int i = 0; i < 1;) {
     scanf("%d", &n);
-    if(n!=0){
-    if (n % 2 != 0) {
-      n++;
+    if (n != 0) {
+      if (n % 2 != 0)
+        n++;
       for (int j = 0; j < 5; j++) {
         count += n;
         n += 2;
       }
-    } else if (n % 2 == 0) {
-    for (int j = 0; j < 5; j++) {
-      count += n;
-      n += 2;
-      }
-    }
-    printf("%d\n", count);
-    count = 0;
-  }
-    else 
+      printf("%d\n", count);
+      count = 0;
+    } else
       i++;
-}
+  }
   return 0;
 }
